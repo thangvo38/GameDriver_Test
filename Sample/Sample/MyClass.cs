081a3adaaf6838c951863d35bc8fa23942de78c9
Thang Vo <thangvo8797@gmail.com>
2023-07-05 10:47:01 +0700

    CLI sample

--- Sample/Sample/MyClass.cs
+++ Sample/Sample/MyClass.cs
@@ -13,6 +13,8 @@ namespace DemoTest
          * Example: mono ../../ext/nunit3-console.exe ./Sample.dll --testparam:Mode=standalone --testparam:pathToExe=/Users/user/Desktop/GameDriver_Demo.app/Contents/MacOS/GameDriver_Demo
          */
         public string testMode = TestContext.Parameters.Get("Mode", "IDE");
+        public string testHost = TestContext.Parameters.Get("Host", "localhost");
+        public string executablePath = TestContext.Parameters.Get("executablePath", @"C:\Users\user\Builds\UnityBuild.exe");
         public string pathToExe = TestContext.Parameters.Get("pathToExe", null); // replace null with the path to your executable as needed, or via the command line as shown above
 
         // Here we initialize the ApiClient
@@ -28,21 +30,20 @@ namespace DemoTest
                 // First we need to create an instance of the ApiClient
 
                 // If an executable path was supplied, we will launch the standalone game
-                if (pathToExe != null)
+                if (executablePath != null && testMode == "standalone") // launches a standalone executable
                 {
-                    ApiClient.Launch(pathToExe);
-                    api.Wait(3000); // Give the executable time to load
-                    api.Connect("localhost", 19734, false, 30);
+                    int PID = ApiClient.Launch(executablePath);
+                    Console.WriteLine($"Launching standalone executable with PID: {PID}");
+                    api.Wait(5000);
+                    api.Connect(testHost, 19734, false, 30);
                 }
-
-                // If no executable path was given, we will attempt to connect to the Unity editor and initiate Play mode
-                else if (testMode == "IDE")
+                else if (executablePath == null && testMode == "IDE") // connects to the IDE and starts Play mode
                 {
-                    api.Connect("localhost", 19734, true, 30);
+                    api.Connect(testHost, 19734, true, 30);
                 }
-                // Otherwise, attempt to connect to an already playing game
-                else api.Connect("localhost", 19734, false, 30);
-
+                else {
+                    api.Connect(testHost, 19734, false, 30); // connects to an already running instance of the editor or standalone build, mobile device, etc...
+                }
             }
             catch (Exception e)
             {
